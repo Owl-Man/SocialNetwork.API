35eddc6435f2992470fd1e0ee481df7b953f072d
Fire Owl <61467254+Owl-Man@users.noreply.github.com>
2025-03-02 22:49:03 +0300

    Added coontroller & response model

---
+++ SocialNetwork.API/Contracts/PostsResponse.cs
@@ -0,0 +1,5 @@
+﻿using SocialNetwork.Core.Models;
+
+namespace SocialNetwork.API.Contracts;
+
+public record PostsResponse(Guid id, string title, string content, Guid authorId, Topic topic);
--- SocialNetwork.API/Controllers/PostsController.cs
+++ SocialNetwork.API/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.API.Contracts;
 using SocialNetwork.Application.Services;
 
 namespace SocialNetwork.API.Controllers;
@@ -12,5 +13,15 @@ public class PostsController : ControllerBase
     public PostsController(IPostsService postsService)
     {
         _postsService = postsService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<PostsResponse>>> GetPosts() 
+    {
+        var posts = await _postsService.GetAllPosts();
+
+        var response = posts.Select(p => new PostsResponse(p.Id, p.Title, p.Content, p.AuthorId, p.Topic));
+
+        return Ok(response);
     }
 }
